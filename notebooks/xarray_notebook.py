b5f0365a0ccdbcaffd1e79394755b1984935aa24
Phil Austin <paustin@eos.ubc.ca>
2019-11-23 14:21:32 -0800

    fix xarray naming bug

--- notebooks/xarray_notebook.py
+++ notebooks/xarray_notebook.py
@@ -3,12 +3,13 @@
 # jupyter:
 #   jupytext:
 #     cell_metadata_filter: all
+#     cell_metadata_json: true
 #     notebook_metadata_filter: all,-language_info
 #     text_representation:
 #       extension: .py
 #       format_name: percent
-#       format_version: '1.2'
-#       jupytext_version: 1.2.4
+#       format_version: '1.3'
+#       jupytext_version: 1.3.0
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
